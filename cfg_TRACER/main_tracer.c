d59103f99eba9ce5d84b8ffa6c5421bea67afd38
smuuze <sebastia_n@web.de>
2022-05-26 14:04:15 +0200

    MOD: main_tracer - replaced /t by blanks

--- cfg_TRACER/main_tracer.c
+++ cfg_TRACER/main_tracer.c
@@ -68,17 +68,17 @@
 // --------------------------------------------------------------------------------
 
 #ifndef TRACER_DEFAULT_COM_DRIVER_DEVICE
-#define TRACER_DEFAULT_COM_DRIVER_DEVICE		"/dev/serial0"
+#define TRACER_DEFAULT_COM_DRIVER_DEVICE            "/dev/serial0"
 #endif
 
 // --------------------------------------------------------------------------------
 
 #ifndef TRACER_RAW_TRACE_OBJECT_QEUE_SIZE
-#define TRACER_RAW_TRACE_OBJECT_QEUE_SIZE		100
+#define TRACER_RAW_TRACE_OBJECT_QEUE_SIZE           100
 #endif
 
 #ifndef TRACER_PARSED_TRACE_OBJECT_QEUE_SIZE
-#define TRACER_PARSED_TRACE_OBJECT_QEUE_SIZE		100
+#define TRACER_PARSED_TRACE_OBJECT_QEUE_SIZE        100
 #endif
 
 // --------------------------------------------------------------------------------
@@ -157,79 +157,79 @@ static TRX_DRIVER_CONFIGURATION driver_cfg;
  */
 int main(int argc, char* argv[]) {
 
-	ATOMIC_OPERATION
-	(
-		initialization();
+    ATOMIC_OPERATION
+    (
+        initialization();
 
-		READ_TRACE_OBJECT_THREAD_init();
-		PARSE_TRACE_OBJECT_THREAD_init();
-		PRINT_TRACE_OBJECT_THREAD_init();
-	)
+        READ_TRACE_OBJECT_THREAD_init();
+        PARSE_TRACE_OBJECT_THREAD_init();
+        PRINT_TRACE_OBJECT_THREAD_init();
+    )
 
-	{
-		DEBUG_PASS("main() - Initialize Slots");
+    {
+        DEBUG_PASS("main() - Initialize Slots");
 
-		DEBUG_PASS("main() - MAIN_CLI_HELP_REQUESTED_SLOT_connect()");
-		MAIN_CLI_HELP_REQUESTED_SLOT_connect();
+        DEBUG_PASS("main() - MAIN_CLI_HELP_REQUESTED_SLOT_connect()");
+        MAIN_CLI_HELP_REQUESTED_SLOT_connect();
 
-		DEBUG_PASS("main() - MAIN_CLI_INVALID_PARAMETER_SLOT_connect()");
-		MAIN_CLI_INVALID_PARAMETER_SLOT_connect();
+        DEBUG_PASS("main() - MAIN_CLI_INVALID_PARAMETER_SLOT_connect()");
+        MAIN_CLI_INVALID_PARAMETER_SLOT_connect();
 
-		DEBUG_PASS("main() - MAIN_CLI_UNKNOWN_ARGUMENT_SLOT_connect()");
-		MAIN_CLI_UNKNOWN_ARGUMENT_SLOT_connect();
+        DEBUG_PASS("main() - MAIN_CLI_UNKNOWN_ARGUMENT_SLOT_connect()");
+        MAIN_CLI_UNKNOWN_ARGUMENT_SLOT_connect();
 
-		DEBUG_PASS("main() - MAIN_CLI_NO_ARGUMENT_GIVEN_SLOT_connect()");
-		MAIN_CLI_NO_ARGUMENT_GIVEN_SLOT_connect();
+        DEBUG_PASS("main() - MAIN_CLI_NO_ARGUMENT_GIVEN_SLOT_connect()");
+        MAIN_CLI_NO_ARGUMENT_GIVEN_SLOT_connect();
 
-		DEBUG_PASS("main() - MAIN_CLI_ARGUMENT_DEVICE_SIGNAL_SLOT_connect()");
-		MAIN_CLI_ARGUMENT_DEVICE_SIGNAL_SLOT_connect();
-	}
+        DEBUG_PASS("main() - MAIN_CLI_ARGUMENT_DEVICE_SIGNAL_SLOT_connect()");
+        MAIN_CLI_ARGUMENT_DEVICE_SIGNAL_SLOT_connect();
+    }
 
-	{
-		DEBUG_PASS("main() - Initialize communication driver");
+    {
+        DEBUG_PASS("main() - Initialize communication driver");
 
-		driver_cfg.module.usart.baudrate = BAUDRATE_230400;
-		driver_cfg.module.usart.databits = DATABITS_8;
-		driver_cfg.module.usart.parity = PARITY_NONE;
-		driver_cfg.module.usart.stopbits = STOPBITS_1;
+        driver_cfg.module.usart.baudrate = BAUDRATE_230400;
+        driver_cfg.module.usart.databits = DATABITS_8;
+        driver_cfg.module.usart.parity = PARITY_NONE;
+        driver_cfg.module.usart.stopbits = STOPBITS_1;
 
-		common_tools_string_clear(driver_cfg.device.name, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
-		common_tools_string_copy_string(driver_cfg.device.name, TRACER_DEFAULT_COM_DRIVER_DEVICE, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
-	}
+        common_tools_string_clear(driver_cfg.device.name, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
+        common_tools_string_copy_string(driver_cfg.device.name, TRACER_DEFAULT_COM_DRIVER_DEVICE, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
+    }
 
-	command_line_interface(argc, argv);
+    command_line_interface(argc, argv);
 
-	if (exit_program) {
-		DEBUG_PASS("main() - PROGRAM EXIT REQUESTED !!! ---");
-		return 1;
-	}
-	
-	RAW_TRACE_OBJECT_QEUE_init();
-	TRACE_OBJECT_QEUE_init();
+    if (exit_program) {
+        DEBUG_PASS("main() - PROGRAM EXIT REQUESTED !!! ---");
+        return 1;
+    }
+    
+    RAW_TRACE_OBJECT_QEUE_init();
+    TRACE_OBJECT_QEUE_init();
 
 
-	i_system.driver.usart0->initialize();
-	i_system.driver.usart0->configure(&driver_cfg);
-	i_system.driver.usart0->start_rx(TRX_DRIVER_INTERFACE_UNLIMITED_RX_LENGTH);
+    i_system.driver.usart0->initialize();
+    i_system.driver.usart0->configure(&driver_cfg);
+    i_system.driver.usart0->start_rx(TRX_DRIVER_INTERFACE_UNLIMITED_RX_LENGTH);
 
-	thread_read_trace_object_set_com_driver(i_system.driver.usart0);
+    thread_read_trace_object_set_com_driver(i_system.driver.usart0);
 
-	READ_TRACE_OBJECT_THREAD_start();
-	PARSE_TRACE_OBJECT_THREAD_start();
-	PRINT_TRACE_OBJECT_THREAD_start();
+    READ_TRACE_OBJECT_THREAD_start();
+    PARSE_TRACE_OBJECT_THREAD_start();
+    PRINT_TRACE_OBJECT_THREAD_start();
 
-	for (;;) {
+    for (;;) {
 
-		if (exit_program) {
-			break;
-		}
-		
-		mcu_task_controller_schedule();
-		mcu_task_controller_background_run();
-		watchdog();
-	}
+        if (exit_program) {
+            break;
+        }
+        
+        mcu_task_controller_schedule();
+        mcu_task_controller_background_run();
+        watchdog();
+    }
 
-	return 0;
+    return 0;
 }
 
 // --------------------------------------------------------------------------------
@@ -241,15 +241,15 @@ int main(int argc, char* argv[]) {
  */
 static void main_CLI_INVALID_PARAMETER_SLOT_CALLBACK(const void* p_argument) {
 
-	DEBUG_PASS("main_CLI_INVALID_PARAMETER_SLOT_CALLBACK");
+    DEBUG_PASS("main_CLI_INVALID_PARAMETER_SLOT_CALLBACK");
 
-	if (p_argument != NULL) {
-		printf("Invalid parameter for arguemnt %s given!\n", (char*)p_argument);
-	} else {
-		console_write_line("Invalid parameter given, check your input!");
-	}
+    if (p_argument != NULL) {
+        printf("Invalid parameter for arguemnt %s given!\n", (char*)p_argument);
+    } else {
+        console_write_line("Invalid parameter given, check your input!");
+    }
 
-	main_CLI_HELP_REQUESTED_SLOT_CALLBACK(NULL);
+    main_CLI_HELP_REQUESTED_SLOT_CALLBACK(NULL);
 }
 
 /**
@@ -258,25 +258,25 @@ static void main_CLI_INVALID_PARAMETER_SLOT_CALLBACK(const void* p_argument) {
  * @param p_argument 
  */
 static void main_CLI_HELP_REQUESTED_SLOT_CALLBACK(const void* p_argument) {
-	(void) p_argument;
+    (void) p_argument;
 
-	console_write("SHC Tracer Version: ");
+    console_write("SHC Tracer Version: ");
     console_write_number(VERSION_MAJOR);
-	console_write(".");
+    console_write(".");
     console_write_number(VERSION_MINOR);
 
     console_new_line();
     console_new_line();
 
-	console_write_line("Usage: shcTracer [options]]");
-	console_write_line("Options:");
-	console_write_line("-dev <device_file>                 : device to use for reading trace data");
-	console_write_line("-path <path>                       : path to directory that includes your makefile");
-	console_write_line("-file <path>                       : traceoutput will be stored into this file");
-	console_write_line("-console                           : traceoutput will be shown on console");
-	console_write_line("-mqtt <topic>@<servicer_ip:port>   : traceoutput will be shown on console");
+    console_write_line("Usage: shcTracer [options]]");
+    console_write_line("Options:");
+    console_write_line("-dev <device_file>                 : device to use for reading trace data");
+    console_write_line("-path <path>                       : path to directory that includes your makefile");
+    console_write_line("-file <path>                       : traceoutput will be stored into this file");
+    console_write_line("-console                           : traceoutput will be shown on console");
+    console_write_line("-mqtt <topic>@<servicer_ip:port>   : traceoutput will be shown on console");
 
-	exit_program = 1;
+    exit_program = 1;
 }
 
 /**
@@ -286,20 +286,20 @@ static void main_CLI_HELP_REQUESTED_SLOT_CALLBACK(const void* p_argument) {
  */
 static void main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK(const void* p_argument) {
 
-	DEBUG_PASS("main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK()");
+    DEBUG_PASS("main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK()");
 
-	if (p_argument == NULL) {
+    if (p_argument == NULL) {
 
-		DEBUG_PASS("main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK() - NULLPOINTER EXCEPTION");
-		console_write_line("Unknown argument given given!");
+        DEBUG_PASS("main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK() - NULLPOINTER EXCEPTION");
+        console_write_line("Unknown argument given given!");
 
-	} else {
+    } else {
 
-		COMMAND_LINE_ARGUMENT_TYPE* p_unknown_argument = (COMMAND_LINE_ARGUMENT_TYPE*) p_argument;
-		console_write_string("Unknown argument given ", p_unknown_argument->argument);
-	}
+        COMMAND_LINE_ARGUMENT_TYPE* p_unknown_argument = (COMMAND_LINE_ARGUMENT_TYPE*) p_argument;
+        console_write_string("Unknown argument given ", p_unknown_argument->argument);
+    }
 
-	exit_program = 1;
+    exit_program = 1;
 }
 
 /**
@@ -309,11 +309,11 @@ static void main_CLI_UNKNOWN_ARGUMENT_SLOT_CALLBACK(const void* p_argument) {
  */
 static void main_CLI_NO_ARGUMENT_GIVEN_CALLBACK(const void* p_argument) {
 
-	DEBUG_PASS("main_CLI_NO_ARGUMENT_GIVEN_CALLBACK()");
-	console_write_line("No argument was given !");
-	console_write_line("give -help to see a list of valid arguments");
+    DEBUG_PASS("main_CLI_NO_ARGUMENT_GIVEN_CALLBACK()");
+    console_write_line("No argument was given !");
+    console_write_line("give -help to see a list of valid arguments");
 
-	exit_program = 1;
+    exit_program = 1;
 }
 
 /**
@@ -323,14 +323,14 @@ static void main_CLI_NO_ARGUMENT_GIVEN_CALLBACK(const void* p_argument) {
  */
 static void main_CLI_ARGUMENT_DEVICE_SIGNAL_CALLBACK(const void* p_argument) {
 
-	if (p_argument == NULL) {
-		DEBUG_PASS("main_CLI_ARGUMENT_DEVICE_SIGNAL_CALLBACK() - NULL_POINTER_EXCEPTION");
-		return;
-	}
+    if (p_argument == NULL) {
+        DEBUG_PASS("main_CLI_ARGUMENT_DEVICE_SIGNAL_CALLBACK() - NULL_POINTER_EXCEPTION");
+        return;
+    }
 
-	const char* p_string = (const char*)p_argument;
-	DEBUG_TRACE_STR(p_string, "main_CLI_ARGUMENT_DEVICE_SIGNAL_CALLBACK() - Device");
+    const char* p_string = (const char*)p_argument;
+    DEBUG_TRACE_STR(p_string, "main_CLI_ARGUMENT_DEVICE_SIGNAL_CALLBACK() - Device");
 
-	common_tools_string_clear(driver_cfg.device.name, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
-	common_tools_string_copy_string(driver_cfg.device.name, p_string, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
+    common_tools_string_clear(driver_cfg.device.name, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
+    common_tools_string_copy_string(driver_cfg.device.name, p_string, DRIVER_CFG_DEVICE_NAME_MAX_LENGTH);
 }
